3889491464b48d4d10644c6cb869223bd8af75b8
김광용 <veta6287@naver.com>
2022-03-16 22:42:08 +0900

    22.03.16

--- step_quest/step_9/9020.py
+++ step_quest/step_9/9020.py
@@ -1,24 +1,32 @@
-def num(ARR, N, lst):
-    for i in range(2, N):
-        if ARR[i] == 1:
-            lst.append(i)
-            n = 2*i
-            while n <= N-1:
-                ARR[n] = 0
-                n += i
-    return lst
+def num(N):
+    # 찾는 수가 소수리스트의 마지막 수보다 작으면 pass
+    if lst[-1] > N:
+        pass
+    else:
+        for i in range(lst[-1], N):
+            for j in lst:
+                is_num = True
+                if i % j == 0:
+                    # ARR[i] = 0
+                    is_num = False
+                    break
+            if is_num == True and i not in lst:
+                lst.append(i)
+
 T = int(input())
+# 소수 담기
+lst = [2]
+# 전체 수 리스트
+# ARR = [1] * 10001
 for tc in range(T):
     N = int(input())
-    ARR = [1]*N
-    lst = []
-    num(ARR, N, lst)
+    num(N)
     min_num = 10000000000
-    for i in lst:
-        for j in lst:
-            if i + j == N and i <= j and min_num > j - i:
-                min_num = j - i
-                ans_x = i
-                ans_y = j
+    h = N//2
+    for i in range(h):
+        if (h - i) in lst and (N - h + i) in lst:
+            ans_x = h - i
+            ans_y = N - h + i  
+            break
 
     print(f'{ans_x} {ans_y}')
