da7490cee09eeff2a6c93f1280f84a7129553fae
김광용 <veta6287@naver.com>
2022-03-28 20:23:42 +0900

    22.03.28

--- rank/silver/2548.py
+++ rank/silver/2548.py
@@ -1,22 +1,19 @@
 N = int(input())
 lst = list(map(int, input().split()))
 lst.sort()
-lst.insert(0, 1)
-lst.append(20000)
-result = 100000000000
-num = 20001
-for i in range(N+1):
-    for j in range(lst[i], lst[i+1]):
-        val = 0
-        for k in lst:
-            val += abs(k-j)
-            if val > result:
-                break
-        if val < result:
-            result = val
-            num = j
-        elif val == result and num > j:
-            num = j
+result = 10000000000
+num = 20000
+for i in lst:
+    val = 0
+    for j in lst:
+        val += abs(i-j)
+        if val > result:
+            break
+    if val < result:
+        result = val
+        num = i
+    elif val == result and num > i:
+        num = i
 
 print(num)
 
