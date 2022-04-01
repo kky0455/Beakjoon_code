0038000a85acadc6c43d0ba8cc860ecc5b879b2a
김광용 <veta6287@naver.com>
2022-04-01 22:29:56 +0900

    22.04.01

--- rank/silver/1018.py
+++ rank/silver/1018.py
@@ -1,8 +1,35 @@
-d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+def find(y, x):
+    global result
+    cnt = 0
+    block = arr[y][x]
+    for i in range(8):
+        if i % 2:
+            for j in range(8):
+                if j % 2:
+                    if arr[y+i][x+j] != block:
+                        cnt += 1
+                else:
+                    if arr[y+i][x+j] == block:
+                        cnt += 1
+        else:
+            for j in range(8):
+                if j % 2:
+                    if arr[y+i][x+j] == block:
+                        cnt += 1
+                else:
+                    if arr[y+i][x+j] != block:
+                        cnt += 1
+    if cnt < result:
+        result = cnt
+    if 64 - cnt < result:
+        result = 64 - cnt
 
-M, N = map(int, input().split())
-ARR = []
-for _ in range(N):
-    ARR.append(list(map(str, input())))
-
-
+N, M = map(int, input().split())
+result = 1000000000
+arr = []
+for i in range(N):
+    arr.append(list(input()))
+for i in range(0, N-7):
+    for j in range(0, M-7):
+        find(i, j)
+print(result)
