a0a7b71dc4f13f22ffbac8c7ccfd6af1a9b6122c
김광용 <veta6287@naver.com>
2022-05-09 01:49:36 +0900

    22.05.09

--- rank/gold/1219.py
+++ rank/gold/1219.py
@@ -2,46 +2,31 @@ import heapq
 import sys
 input = sys.stdin.readline
 
-def dijk():
-    D = [-1000000000000000000] * N
-    q = []
+def bell():
+    D = [-1000000000] * N
     D[S] = earn[S]
-    heapq.heappush(q, (earn[S], S))
-    visited = [0] * N
-    visited[S] = 1
-    while q:
-        dis, now = heapq.heappop(q)
-        for e, d in G[now]:
-            if D[e] < dis + earn[e] - d:
-                D[e] = dis + earn[e] - d
-                visited[e] += 1
-                if visited[e] == 1:
-                    heapq.heappush(q, (D[e], e))
-    if D[E] == -1000000000000000000:
-        return print('gg')
-
-    loop_lst = []
-    for i in range(len(visited)):
-        if visited[i] > 1:
-            loop_lst.append(i)
-    if loop_lst:
-        for val in loop_lst:
-            V = [-1000000000000000000] * N
-            V[val] = 0
-            heapq.heappush(q, (V[val], val))
-            visited = [0] * N
-            while q:
-                dis, now = heapq.heappop(q)
-                for e, d in G[now]:
-                    if V[e] < dis + earn[e] - d:
-                        V[e] = dis + earn[e] - d
-                        visited[e] += 1
-                        if visited[e] == 1:
-                            heapq.heappush(q, (V[e], e))
-            if visited[E] > 0:
-                return print('Gee')
-    print(D[E])
-
+    for i in range(N+1):
+        if i == N and D[E] == -1000000000:
+            return print('gg')
+        for j in range(N):
+            if D[j] == -1000000000:
+                continue
+            for e, d in G[j]:
+                if D[e] < D[j] + earn[e] - d:
+                    D[e] = D[j] + earn[e] - d
+                    if i == N:
+                        visited = [0] * N
+                        q = []
+                        heapq.heappush(q, e)
+                        while q:
+                            now = heapq.heappop(q)
+                            if now == E:
+                                return print('Gee')
+                            visited[now] = 1
+                            for e, d in G[now]:
+                                if visited[e] == 0:
+                                    heapq.heappush(q, e)
+    return print(D[E])
 
 N, S, E, M = map(int, input().split())
 G = [[] for _ in range(N)]
@@ -49,4 +34,4 @@ for _ in range(M):
     s, e, d = map(int, input().split())
     G[s].append((e, d))
 earn = list(map(int, input().split()))
-dijk()
+bell()
