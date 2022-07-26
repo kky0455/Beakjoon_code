5ca7748c466506ca3aebce2a00461223a988b8cb
Gwangyong <veta6287@naver.com>
2022-07-27 00:31:43 +0900

    22.07.28

--- rank/gold/1722.py
+++ rank/gold/1722.py
@@ -7,26 +7,26 @@ fac = [0] * (N+1)
 ans = []
 if quest[0] == 1:
     num = N-1
-    while quest[1] != 0 :
-        val = math.factorial(num)
-        quo, rem = divmod(quest[1], val)
+    ans = []
+    while quest[1] > 0:
         cnt = 0
+        que, rem = divmod(quest[1], math.factorial(num))
         for i in range(1, N+1):
             if not numLst[i]:
                 cnt += 1
-                if rem > 0:
-                    if quo+1 == cnt:
+                if rem > 0 :
+                    if cnt == que+1:
                         numLst[i] = 1
                         ans.append(i)
                         break
-                elif rem == 0:
-                    if quo == cnt:
+                else:
+                    if cnt == que:
                         numLst[i] = 1
                         ans.append(i)
                         break
-        quest[1] %= val
+        quest[1] = rem
         num -= 1
-    for i in range(1, N+1):
+    for i in range(N, 0, -1):
         if not numLst[i]:
             ans.append(i)
     print(*ans)
